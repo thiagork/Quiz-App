c8d5ddbfa0149d6c75a4fb6c75874b4cf26506f4
Thiago Krüger <thiagork@msn.com>
2019-07-25 09:41:01 +0200

    created questions component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { fromEventPattern } from 'rxjs';
 import { ResultsComponent } from './results/results.component';
+import { QuestionsComponent } from './questions/questions.component';
+
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionFormComponent,
-    ResultsComponent
+    ResultsComponent,
+    QuestionsComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule
